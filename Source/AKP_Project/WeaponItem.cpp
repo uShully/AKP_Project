836bb871b79acbd3217728382ec130c8ca1184a1
uShully <86033758+uShully@users.noreply.github.com>
2023-06-03 18:31:48 +0900

    weapon code fix

---
+++ Source/AKP_Project/WeaponItem.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponItem.h"
+#include "AKP_ProjectCharacter.h"
+#include "CharacterStatComponent.h"
+
+void UWeaponItem::Use(class AAKP_ProjectCharacter* Character)
+{
+	if (Character) {
+		
+	}
+}
